ee4297699b5de58c931a5923f2fc300416d57524
willdoyle757 <61399895+willdoyle757@users.noreply.github.com>
2022-04-01 15:39:30 -0400

    Rect

--- src/com/company/Rectangle.java
+++ src/com/company/Rectangle.java
@@ -5,6 +5,7 @@ public class Rectangle {
     double length;
     double width;
 
+    //constr
     public Rectangle(double length, double width){
         this.length = length;
         this.width = width;
