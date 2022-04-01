cbf9120ab4c529b2694787e9287ac2644a718516
willdoyle757 <61399895+willdoyle757@users.noreply.github.com>
2022-04-01 15:37:02 -0400

    Rect

---
+++ src/com/company/Rectangle.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Rectangle {
+
+    double length;
+    double width;
+
+    public Rectangle(double length, double width){
+        this.length = length;
+        this.width = width;
+    }
+
+    double getPerimiter(){
+        return (2 * (this.width)) + (2 * (this.length));
+    }
+
+    double getArea(){
+        return (this.length * this.width);
+    }
+}
